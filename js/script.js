6d65c10c6c0f99793cbdf308881ec907adb18102
Agus Sanchez <cuartitoup@gmail.com>
2025-10-02 17:44:44 -0300

    primer guardado

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//declaracion
+let cliente;
+
+
+//asignacion de valores
+cliente = "Carlos";
+cliente = "Ana";
+cliente = "Agustin";
+console.log(cliente);
+
+//operaciones
+
+//inicializar variables
+let edad = 37;
+console.log(edad);
+
+//const: variable inmodificable
+const nacimiento = 1986;
+console.log(nacimiento);
